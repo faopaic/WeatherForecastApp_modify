3ea406c0d1262b7cd333e4e09721930b3c2b59ad
kondou naoto <oonukikonuki11@gmail.com>
2025-05-21 11:07:24 +0900

    HTML化出来ました

--- WeatherForecastApp.java
+++ WeatherForecastApp.java
@@ -11,6 +11,9 @@ import java.util.List;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 /**
  * 天気予報アプリ - 本体
  * このアプリケーションは、気象庁のWeb APIから大阪府の天気予報データを取得取得して表示します
@@ -18,35 +21,36 @@ import org.json.JSONObject;
  * @author n.katayama
  * @version 1.0.1
  */
-public class WeatherForecastApp {
-    private static final String TARGET_URL = "https://www.jma.go.jp/bosai/forecast/data/forecast/270000.json";
 
-    public static void main(String[] args) {
-        WeatherApiClient client = new WeatherApiClient(TARGET_URL);
-        try {
-            for (WeatherForecast forecast : client.fetchWeatherForecasts()) {
-                // 日付を yyyy/MM/dd → yyyy年M月d日 に変換
-                String[] dateParts = forecast.getDateTime().split("/");
-                if (dateParts.length == 3) {
-                    int year = Integer.parseInt(dateParts[0]);
-                    int month = Integer.parseInt(dateParts[1]);
-                    int day = Integer.parseInt(dateParts[2].split(" ")[0]);
-                    String weather = forecast.getWeather();
-                    // 天気情報が日付の後ろに含まれている場合の対応
-                    if (forecast.getDateTime().contains(" ")) {
-                        weather = forecast.getDateTime().substring(forecast.getDateTime().indexOf(" ") + 1);
+
+    public class WeatherForecastApp {
+        private static final String TARGET_URL = "https://www.jma.go.jp/bosai/forecast/data/forecast/270000.json";
+
+        public static void main(String[] args) {
+            WeatherApiClient client = new WeatherApiClient(TARGET_URL);
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter("weather.html"))) {
+                writer.write("<html><head><meta charset=\"UTF-8\"><title>天気予報</title></head><body>\n");
+                for (WeatherForecast forecast : client.fetchWeatherForecasts()) {
+                    String[] dateParts = forecast.getDateTime().split("/");
+                    if (dateParts.length == 3) {
+                        int year = Integer.parseInt(dateParts[0]);
+                        int month = Integer.parseInt(dateParts[1]);
+                        int day = Integer.parseInt(dateParts[2].split(" ")[0]);
+                        String weather = forecast.getWeather();
+                        if (forecast.getDateTime().contains(" ")) {
+                            weather = forecast.getDateTime().substring(forecast.getDateTime().indexOf(" ") + 1);
+                        }
+                        writer.write(year + "年" + month + "月" + day + "日 の天気は " + weather + " です。<br>\n");
+                    } else {
+                        writer.write(forecast.getDateTime() + " " + forecast.getWeather() + "<br>\n");
                     }
-                    System.out.println(year + "年" + month + "月" + day + "日 の天気は " + weather + " です。");
-                } else {
-                    // フォールバック（元の出力）
-                    System.out.println(forecast.getDateTime() + " " + forecast.getWeather());
                 }
+                writer.write("</body></html>");
+            } catch (Exception e) {
+                System.out.println("天気予報の取得に失敗しました: " + e.getMessage());
             }
-        } catch (Exception e) {
-            System.out.println("天気予報の取得に失敗しました: " + e.getMessage());
         }
     }
-}
 
 class WeatherApiClient {
     private final String targetUrl;
